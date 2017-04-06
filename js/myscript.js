c7f335d0e7d46fb3425bb3c4be9cbcdbaf4238db
chadgauth <chadgauth@gmail.com>
2017-04-06 13:27:03 -0600

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,8 @@
-var $introduction = $('<div class="announcement delay-500">This is a story of boy meets boy.</div>' +
+var $introduction = $('<div class="announcement-container">' + 
+                      '<div class="announcement delay-500">This is a story of boy meets boy.</div>' +
                       '<div class="announcement delay-1000">But you should know upfront,</div>' +
-                      '<div class="announcement delay-1500">This is not a traditional love story.</div>');
+                      '<div class="announcement delay-1500">This is not a traditional love story.</div>' +
+                      '</div>);
 
 var startOver = (function start(){
     $('body').append($introduction);
