ead224a454da61aa109143fe9ea150c2451f1642
chadgauth <chadgauth@gmail.com>
2017-04-06 13:37:54 -0600

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,12 @@
 var $introduction = $('<div class="announcement-container">' + 
-                      '<div class="announcement delay d500">This is a story of boy meets boy.</div>' +
-                      '<div class="announcement delay d1000">But you should know upfront,</div>' +
-                      '<div class="announcement delay d1500">This is not a traditional love story.</div>' +
+                      '<div class="announcement delay 500">This is a story of boy meets boy.</div>' +
+                      '<div class="announcement delay 1000">But you should know upfront,</div>' +
+                      '<div class="announcement delay 1500">This is not a traditional love story.</div>' +
                       '</div>');
 
-var startOver = (function start(){
-    $('body').append($introduction);
+function start($screenContent){
+    $('body').append($screenContent);
     return start; //return the function itself to reference
-}());
+}();
+
+$(start($introduction));
