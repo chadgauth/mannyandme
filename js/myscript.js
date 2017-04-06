00292251c81045d9c2d01ca7a898835c350be43c
chadgauth <chadgauth@gmail.com>
2017-04-06 16:57:39 -0600

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -2,11 +2,19 @@ var $introduction = $('<div class="announcement-container">' +
                       '<div class="announcement delay 0">This is a story of boy meets boy.</div>' +
                       '<div class="announcement delay 4310">But you should know upfront,</div>' +
                       '<div class="announcement delay 8700">this is not a traditional love story.</div>' +
-                      '</div>');
+                      '</div>').bind("click", function(){ $('meta[name="theme-color"]')[0].content = "#00FF00"; });
 
-function start($screenContent){
+function start($screenContent, hasDelays){
     $('body').append($screenContent);
-    var $delay = $('.delay');
+    if (hasDelays) executeDelays(); 
+};
+
+function delayDisplay($content, delay){
+    setTimeout(function(){$content.css("opacity", 1)}, delay); 
+};
+
+function executeDelays(){
+ var $delay = $('.delay');
     var delays = [];
     for(d in $delay){
       if ($delay[d].classList != undefined)
@@ -14,13 +22,8 @@ function start($screenContent){
     }
     for(dIdx in delays){
        delayDisplay($($delay[dIdx]), parseInt(delays[dIdx]));
-    }
-    return start; //return the function itself to reference
-};
-
-function delayDisplay($content, delay){
-    setTimeout(function(){$content.css("opacity", 1)}, delay); 
-};
+    } 
+}
 
 $($('audio')[0].play());
 $(start($introduction));
