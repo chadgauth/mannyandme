4fa293815ed743a07f0d07023ee385ed6955267d
chadgauth <chadgauth@gmail.com>
2017-04-06 16:45:02 -0600

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,7 @@
 var $introduction = $('<div class="announcement-container">' + 
                       '<div class="announcement delay 0">This is a story of boy meets boy.</div>' +
-                      '<div class="announcement delay 4810">But you should know upfront,</div>' +
-                      '<div class="announcement delay 6700">this is not a traditional love story.</div>' +
+                      '<div class="announcement delay 4310">But you should know upfront,</div>' +
+                      '<div class="announcement delay 8700">this is not a traditional love story.</div>' +
                       '</div>');
 
 function start($screenContent){
