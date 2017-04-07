52785bc8c34ae5eb81187e49b972cc8c1ff513d8
chadgauth <chadgauth@gmail.com>
2017-04-06 21:58:41 -0600

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,10 @@
 var $introduction = $('<div class="announcement-container full">' + 
-                      '<div class="announcement delay 0">This is a story of boy meets boy.</div>' +
-                      '<div class="announcement delay 4310">But you should know upfront,</div>' +
-                      '<div class="announcement delay 8700">this is not a traditional love story.</div>' +
+                      '<div class="announcement delay 0 opacity 1 start-hidden fade-slow">This is a story of boy meets boy.</div>' +
+                      '<div class="announcement delay 4310 opacity 1 start-hidden fade-slow">But you should know upfront,</div>' +
+                      '<div class="announcement delay 7700 opacity 1 start-hidden fade-slow">this is not a traditional love story.</div>' +
                       '</div>');
-var $introGo = $('<div class="heart-icon delay 12000" id="heart"></div>' +
-                 '<div class="heart delay 12000">continue</div>')
+var $introGo = $('<div class="heart-icon delay 9000 opacity 1 start-hidden fade-slow" id="heart"></div>' +
+                 '<div class="heart delay 9000 opacity 1 start-hidden fade-slow">continue</div>')
               .bind("click", function(){ $('meta[name="theme-color"]')[0].content = "#86B094"; start($phoneScreen) });
 $introduction.append($introGo);
 
@@ -16,21 +16,25 @@ function start($content){
     executeDelays(); 
 };
 
-function delayDisplay($content, delay){
-    setTimeout(function(){$content.css("opacity", 1)}, delay); 
-};
-
 function executeDelays(){
  var $delay = $('.delay');
     var delays = [];
+    var props = [];
+    var values = [];
     for(d in $delay){
       if ($delay[d].classList != undefined)
-        delays.push(($delay[d].classList[2]));
+      delays.push(($delay[d].classList[2]));
+      props.push(($delay[d].classList[3]));
+      values.push(($delay[d].classList[4]));
     }
-    for(dIdx in delays){
-       delayDisplay($($delay[dIdx]), parseInt(delays[dIdx]));
+    for(idx in delays){
+       delayCSS($($delay[idx]), parseInt(delays[idx]), props[idx], values[idx]);
     } 
 }
+
+function delayCSS($content, delay, cssProp, value){
+    setTimeout(function(){$content.css(cssProp, value)}, delay); 
+};
 
 $($('audio')[0].play());
 $(start($introduction));
